d5d23b0cec1339860160304e8c3b9539274dee23
mulahas77@gmail.com <mulahas77@gmail.com>
2021-10-17 02:58:04 +0000

    test

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -5,6 +5,7 @@ select
     status,
 
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount,
+    ---original logic before macro was amount / 100 as amount
+    {{ cents_to_dollars('payment_amount', 4 ) }} as amount,
     created as created_at
     from `key-acre-326416.stripe_payments.stripe_payments`
